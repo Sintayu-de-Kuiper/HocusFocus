1fa4886179dd56b1b71b4ebf4d460f9d4f256dc0
Sintayu-de-Kuiper <sintayudekuiper@gmail.com>
2024-07-02 11:06:13 +0200

    Added a BlacklistConfig class

---
+++ src/HocusFocus/HocusFocus.Core/BlacklistConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HocusFocus.Core
+{
+    public class BlacklistConfig
+    {
+        public List<string>? BlockedApps { get; set; }
+    }
+}
